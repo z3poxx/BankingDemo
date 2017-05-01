3e7646a6708c748eab468a6f7a8ed5579661b608
Rikard Bergqvist <rikard.bergqvist@live.com>
2017-05-01 17:37:02 +0200

    Added a basiuc consol application as a base to build the demo application on.

--- WintDemo/Program.cs
+++ WintDemo/Program.cs
@@ -10,6 +10,30 @@ namespace WintDemo
     {
         static void Main(string[] args)
         {
+            bool runDemoApp = true;
+
+            Messages.Greet(); 
+            Messages.ExplainBasicControls();
+                        
+
+            while (runDemoApp)
+            {
+                ConsoleKeyInfo consoleInput = Console.ReadKey();
+
+
+                if (consoleInput.KeyChar == '0')
+                {
+                    Messages.ExplainBasicControls();
+                }
+
+
+                if (consoleInput.KeyChar == '1')
+                {
+                    Messages.TempMSG();
+                    //do stuff
+                }
+
+            }
 
         }
     }
