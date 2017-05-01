5f18525f7454d543a60247b76e2599f06b04ea27
z3poxx <z3poxx@gmail.com>
2017-05-01 17:38:19 +0200

    Added a basiuc consol application as a base to build the demo application on. (#1)

---
+++ WintDemo/messages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WintDemo
+{
+    public static class Messages
+    {
+        public static void Greet()
+        {
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+            Console.WriteLine("Welcome to the WintDemo application");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+        }
+
+        public static void ExplainBasicControls()
+        {
+            Console.WriteLine();
+            Console.WriteLine("These are the basic controls:");
+            Console.WriteLine("Press 0 to display all controls.");
+            Console.WriteLine("Press 1 to display alla ***");
+        }
+
+        public static void TempMSG()
+        {
+            Console.WriteLine("A place holder message:");
+        }
+    }
+}
