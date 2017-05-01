8b2a7e6ee025b20305e094fdd39b178d6805d4bc
Rikard Bergqvist <rikard.bergqvist@live.com>
2017-05-01 13:11:51 +0200

    Add project files.

---
+++ WintDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WintDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
